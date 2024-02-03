440dc80db61310f06f0ed6a495c1aea13a766b50
João Vitor Deon <joaovitordeon@gmail.com>
2024-02-03 15:34:32 -0300

    Update general_metrics.py

--- pages/general_metrics.py
+++ pages/general_metrics.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 from streamlit_extras.switch_page_button import switch_page
-from app import load_data, load_info_sidebar, plot_title
+from app import load_info_sidebar, plot_title
 
 
 def formatar_reais(valor):
@@ -15,7 +15,7 @@ else:
     plot_title()
 
     #load dataset
-    df = load_data()
+    df = st.session_state['data']
 
     # ------------------------------------------------- BIG NUMBERS ----------------------------------------------------------------
     grid = st.columns([3, 3, 3, 3])
